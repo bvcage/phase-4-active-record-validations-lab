7d5e2df1dfd49b3934b92dd24ee1b6f2dddfac8f
Bailey Cage <bailey.cage@gmail.com>
2022-10-13 15:44:22 -0400

    commit tree ce6d6480fd068073f0699c957f322955f8e1ff99

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,19 @@
 class Post < ApplicationRecord
+   validates :title, presence: true
+   validates :content, length: {minimum: 250}
+   validates :summary, length: {maximum: 250}
+   validates :category, inclusion: {in: %w(Fiction Non-Fiction)}
+   validate :validate_is_clickbaity
+
+   def validate_is_clickbaity
+      return if title.nil?
+      clickbait = [
+         /Won't Believe/,
+         /Secret/,
+         /Top \d/,
+         /Guess/
+      ]
+      is_valid = clickbait.any? { |string| self.title.match?(string) }
+      errors.add(:title, "not sufficiently clickbait-y") unless is_valid
+   end
 end
